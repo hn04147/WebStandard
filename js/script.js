fe1e364026b9559a04a6d38c5297b15661b9dfe8
Sangjin Lee <hn04147@naver.com>
2021-05-10 12:02:05 +0900

    2021.5.10

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+//배너
+//html 마크업 세팅 -> css 연동 -> jQuery 연동 -> jQuery 호출
+$(".ban").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true
+});
+
+//버튼을 클릭하면 전체 메뉴 보이게 하기.
 $(".tit .btn").click(function(e){
     e.preventDefault();
     //$("#cont_nav").css("display", "block");
@@ -9,3 +19,20 @@ $(".tit .btn").click(function(e){
     $("#cont_nav").slideToggle(200);
     $("this").toggleClass("on");
 });
+
+//탭메뉴
+var $tab_list = $(".tab_menu");
+$tab_list.find("ul ul").hide();
+$tab_list.find("li.active > ul").show();
+function tabMenu(e){
+    e.preventDefault();
+    var $this = $(this);
+    $this.next("ul").show().parent("li").addClass("active").siblings("li").removeClass("active").find(">ul").hide();
+    /*
+    $this.parent("li").addClass("active");
+    $this.parent().siblings("li").removeClass("active");
+    $this.next("ul").show();
+    $this.parent().siblings("li").find(">ul").hide();
+    */
+}
+$tab_list.find("ul>li>a").click(tabMenu).focus(tabMenu);
