8566c3275006309ea90a7b5844a8af30461aa09c
Sangjin Lee <hn04147@naver.com>
2021-05-10 18:47:36 +0900

    2021.5.10

--- js/script.js
+++ js/script.js
@@ -36,3 +36,56 @@ function tabMenu(e){
     */
 }
 $tab_list.find("ul>li>a").click(tabMenu).focus(tabMenu);
+
+//갤러리
+$(".gallery_img").slick({
+    arrows: false,
+    fade: true,
+    speed:500,
+    autoplaySpeed: 1000,
+    autoplay: true
+});
+$(".play").click(function(){
+    $(".gallery_img").slick("slickPlay")
+});
+$(".pause").click(function(){
+    $(".gallery_img").slick("slickPause")
+});
+$(".prev").click(function(){
+    $(".gallery_img").slick("slickPrev")
+});
+$(".next").click(function(){
+    $(".gallery_img").slick("slickNext")
+});
+
+//레이어 팝업
+$("#layer .close").click(function(e){
+    e.preventDefault();
+    //$("#layer").hide();
+    //$("#layer").css("display","none");
+    //$("#layer").slideUp();
+    $("#layer").fadeOut();
+});
+$(".layer").click(function(e){
+    e.preventDefault();
+    //$("#layer").show();
+    //$("#layer").css("display","block");
+    //$("#layer").slideDown();
+    $("#layer").fadeIn();
+})
+
+//윈도우 팝업
+$(".window").click(function(e){
+    e.preventDefault();
+    //window.open("파일명", "팝업이름", "옵션설정");
+    //옵션: left, top, width, height, status, toolbar, location, menubar, scrollbars, fullscreen;
+    window.open("popup.html", "popup01", "width=800, height=590, left=50, top=50, scrollbars=0, menubar=0, toolbar=0");
+});
+
+//라이트 박스
+$(".lightgallery").lightGallery({
+    speed: 500,
+    thumbnnail: true,
+    autoplay: true,
+    pause: 2000
+});
